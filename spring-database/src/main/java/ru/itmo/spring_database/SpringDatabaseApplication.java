e9c13304e38cb8c4d5397367c5653cdb6aeb6edd
ircho <gorlenkoircho96@mail.ru>
2025-10-02 22:19:41 +0300

    module architecture

---
+++ spring-database/src/main/java/ru/itmo/spring_database/SpringDatabaseApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.spring_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseApplication.class, args);
+	}
+
+}
